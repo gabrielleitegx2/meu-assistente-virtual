2352c80936a348f0607367bacd36535d1b5843e6
gabriel leite <gabriel.leite@gx2.com.br>
2025-07-18 22:51:44 -0300

    Melhora layout do chat e deixa responsivo estilo WhatsApp

--- frontend/static/js/chat.js
+++ frontend/static/js/chat.js
@@ -8,10 +8,10 @@ async function enviarPergunta() {
 
     try {
         // URL PARA USAR LOCAL
-        // const resposta = await fetch('http://127.0.0.1:8000/perguntar', { 
+         const resposta = await fetch('http://127.0.0.1:8000/perguntar', { 
 
         // URL PARA USAR PRODUÇAO
-        const resposta = await fetch('https://meu-assistente-virtual-n74d.onrender.com/perguntar', {
+        //const resposta = await fetch('https://meu-assistente-virtual-n74d.onrender.com/perguntar', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
